fcbdd82dc43756d28374698073d0378d2d816924
Grace de la Mora <thetrend@users.noreply.github.com>
2025-02-08 21:50:30 -0500

    Item Details page

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -33,7 +33,7 @@ const AuthorItems = ({ collection, authorImg, authorId }) => {
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${item.nftId}`}>
                     <img
                       src={item.nftImage}
                       className="lazy nft__item_preview"
@@ -42,7 +42,7 @@ const AuthorItems = ({ collection, authorImg, authorId }) => {
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${item.nftId}`}>
                     <h4>{item.title}</h4>
                   </Link>
                   <div className="nft__item_price">{item.price} ETH</div>
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,84 +1,121 @@
-import React, { useEffect } from "react";
-import EthImage from "../images/ethereum.svg";
+import React, { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
 import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import EthImage from "../images/ethereum.svg";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useParams } from "react-router-dom";
 
 const ItemDetails = () => {
+  const { itemId } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [itemData, setItemData] = useState(null);
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+
+    const fetchItemDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${itemId}`
+        );
+        const data = await response.json();
+        setItemData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching item details:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchItemDetails();
+  }, [itemId]);
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
+        <div id="top" />
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
               <div className="col-md-6 text-center">
-                <img
-                  src={nftImage}
-                  className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
-                />
+                {loading ? (
+                  <Skeleton height={300} width={300} />
+                ) : (
+                  <img
+                    src={itemData?.nftImage}
+                    className="img-fluid img-rounded mb-sm-30 nft-image"
+                    alt={itemData?.title}
+                  />
+                )}
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>{loading ? <Skeleton width={200} /> : itemData?.title}</h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      100
+                      <i className="fa fa-eye" />
+                      {loading ? <Skeleton width={40} /> : itemData?.views}
                     </div>
                     <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      74
+                      <i className="fa fa-heart" />
+                      {loading ? <Skeleton width={40} /> : itemData?.likes}
                     </div>
                   </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
+                  <p>{loading ? <Skeleton count={3} /> : itemData?.description}</p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
-                      <h6>Owner</h6>
+                      <h6>{loading ? <Skeleton width={60} /> : "Owner"}</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
+                          <Link to={`/author/${itemData?.ownerId}`}>
+                            {loading ? (
+                              <Skeleton circle height={40} width={40} />
+                            ) : (
+                              <img className="lazy" src={itemData?.ownerImage} alt={itemData?.ownerName} />
+                            )}
+                            <i className="fa fa-check" />
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${itemData?.ownerId}`}>
+                            {loading ? <Skeleton width={100} /> : itemData?.ownerName}
+                          </Link>
                         </div>
                       </div>
                     </div>
-                    <div></div>
+                    <div />
                   </div>
                   <div className="de_tab tab_simple">
                     <div className="de_tab_content">
-                      <h6>Creator</h6>
+                      <h6>{loading ? <Skeleton width={60} /> : "Creator"}</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
+                            {loading ? (
+                              <Skeleton circle height={40} width={40} />
+                            ) : (
+                              <img className="lazy" src={itemData?.creatorImage} alt={itemData?.creatorName} />
+                            )}
+                            <i className="fa fa-check" />
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to="/author">
+                            {loading ? <Skeleton width={100} /> : itemData?.creatorName}
+                          </Link>
                         </div>
                       </div>
                     </div>
-                    <div className="spacer-40"></div>
-                    <h6>Price</h6>
+                    <div className="spacer-40" />
+                    <h6>{loading ? <Skeleton width={60} /> : "Price"}</h6>
                     <div className="nft-item-price">
-                      <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      {loading ? (
+                        <Skeleton width={40} circle />
+                      ) : (
+                        <img src={EthImage} alt="Ethereum" />
+                      )}
+                      <span>{loading ? <Skeleton width={40} /> : itemData?.price}</span>
                     </div>
                   </div>
                 </div>
